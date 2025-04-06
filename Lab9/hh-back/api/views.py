ea8dd00201335f9d73b4378865a747fde3b0570d
Timur M <136479681+fendcg@users.noreply.github.com>
2025-04-06 18:36:32 +0500

    l9

--- Lab9/hh-back/api/views.py
+++ Lab9/hh-back/api/views.py
@@ -2,18 +2,16 @@ from rest_framework import generics
 from .models import Company, Vacancy
 from .serializers import CompanySerializer, VacancySerializer
 
-# /api/companies - список всех компаний
+
 class CompanyListAPIView(generics.ListAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
-# /api/companies/<int:id>/ - получение одной компании
 class CompanyRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
     lookup_field = 'id'
 
-# /api/companies/<int:id>/vacancies/ - список вакансий по компании
 class CompanyVacanciesAPIView(generics.ListAPIView):
     serializer_class = VacancySerializer
 
@@ -21,18 +19,15 @@ class CompanyVacanciesAPIView(generics.ListAPIView):
         company_id = self.kwargs['id']
         return Vacancy.objects.filter(company_id=company_id)
 
-# /api/vacancies - список всех вакансий
 class VacancyListAPIView(generics.ListAPIView):
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
 
-# /api/vacancies/<int:id>/ - получение одной вакансии
 class VacancyRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
     lookup_field = 'id'
 
-# /api/vacancies/top_ten/ - топ 10 вакансий с наибольшей зарплатой
 class TopTenVacanciesAPIView(generics.ListAPIView):
     serializer_class = VacancySerializer
 
